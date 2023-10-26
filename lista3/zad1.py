6fbd94749246937819d106202f7e4bf244f188ec
Mateusz Budzyński <budzynekm09@gmail.com>
2023-10-26 15:33:59 +0200

    new attributes added to init and load_data method

--- lista3/zad1.py
+++ lista3/zad1.py
@@ -4,9 +4,17 @@ class Zad1:
         self.a_length = 0
         self.b_length = 0
         self.c_length = 0
+        self.table = []
+        self.smallest = 0
+        self.biggest = 0
     def load_data(self):
         tmp_table = input().split()
         self.length_of_ribbon = int(tmp_table[0])
         self.a_length = int(tmp_table[0])
         self.b_length = int(tmp_table[0])
         self.c_length = int(tmp_table[0])
+        self.table = [-1] * self.length_of_ribbon
+        self.smallest = min(self.c_length, self.b_length, self.a_length)
+        self.biggest = max(self.c_length, self.b_length, self.a_length)
+
+
