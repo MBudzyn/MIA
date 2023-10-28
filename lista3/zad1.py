3683cd532f647b5b203aa64cb668ac639245e7ec
Mateusz Budzyński <budzynekm09@gmail.com>
2023-10-28 20:04:30 +0200

    small changes

--- lista3/zad1.py
+++ lista3/zad1.py
@@ -15,14 +15,11 @@ class Zad1:
         self.b_length = 0
         self.c_length = 0
         self.table = []
+        self.mid = 0
         self.smallest = 0
         self.biggest = 0
     def load_data(self):
-        tmp_table = input().split()
-        self.length_of_ribbon = int(tmp_table[0])
-        self.a_length = int(tmp_table[1])
-        self.b_length = int(tmp_table[2])
-        self.c_length = int(tmp_table[3])
+        self.length_of_ribbon,self.a_length,self.b_length,self.c_length = map(int, input().split())
         self.table = [-1] * (self.length_of_ribbon + 1)
         self.smallest = min(self.c_length, self.b_length, self.a_length)
         self.biggest = max(self.c_length, self.b_length, self.a_length)
@@ -37,18 +34,19 @@ class Zad1:
         self.table[self.smallest] = 1
         self.table[self.mid] = 1
         self.table[self.biggest] = 1
+        self.table[0] = 0
         for i in range(self.smallest + 1,self.mid):
             if self.table[i - self.smallest] != -1:
                 self.table[i] = self.table[i - self.smallest] + 1
 
         for i in range(self.mid,self.biggest):
-            if self.table[i - self.mid] != -1:
+            if self.table[i - self.mid] != -1 and self.mid != i:
                 self.table[i] = self.table[i - self.mid] + 1
             if self.table[i - self.smallest] != -1:
                 self.table[i] = self.table[i - self.smallest] + 1
 
         for i in range(self.biggest,self.length_of_ribbon + 1):
-            if self.table[i - self.biggest] != -1:
+            if self.table[i - self.biggest] != -1 and i != self.biggest:
                 self.table[i] = self.table[i - self.biggest] + 1
             if self.table[i - self.mid] != -1:
                 self.table[i] = self.table[i - self.mid] + 1
@@ -58,7 +56,6 @@ class Zad1:
         self.load_data()
         self.table_filling()
         print(self.table[self.length_of_ribbon])
-
 
 zad1 = Zad1()
 zad1.fun()
