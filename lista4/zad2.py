2492b5b05b7ed5b38914c3d94b431ce831a98f39
Mateusz Budzyński <budzynekm09@gmail.com>
2023-10-29 16:34:50 +0100

    problem with last space resolved

--- lista4/zad2.py
+++ lista4/zad2.py
@@ -19,11 +19,12 @@ class Zad2():
         self.number_of_hairs = 0
         self.number_of_command = 0
         self.favorite_number = 0
+        self.result_table = []
     def load_data(self):
         self.number_of_hairs,self.number_of_command,self.favorite_number = map(int,input().split())
         self.table_with_hairs = list(map(int, input().split()))
     def execute_0_command(self):
-        print(smallest_number_of_slices(self.table_with_hairs,self.favorite_number))
+        self.result_table.append(smallest_number_of_slices(self.table_with_hairs,self.favorite_number))
     def execute_1_command(self,index_to_grow,grow_by):
         self.table_with_hairs[index_to_grow-1] += grow_by
     def execute_command(self):
@@ -32,11 +33,21 @@ class Zad2():
             self.execute_0_command()
         else:
             self.execute_1_command(pom_list[1],pom_list[2])
+    def execute_all_commands(self):
+        for command in range(self.number_of_command):
+            self.execute_command()
+    def print_result(self):
+        for i in range(len(self.result_table) - 1):
+            print(self.result_table[i])
+        print(self.result_table[len(self.result_table) - 1], end="")
+
+
+
 
     def run(self):
         self.load_data()
-        for command in range(self.number_of_command):
-            self.execute_command()
+        self.execute_all_commands()
+        self.print_result()
 
 zad2 = Zad2()
 zad2.run()
