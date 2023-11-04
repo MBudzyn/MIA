cb87efb4c08a569f0731bc998395c7eb5896b6d0
Mateusz Budzyński <budzynekm09@gmail.com>
2023-11-04 14:57:18 +0100

    load and init method filled with code

--- lista5/zad3.py
+++ lista5/zad3.py
@@ -1,5 +1,12 @@
 class Zad3():
     def __init__(self):
-        pass
+        self.number_of_marks = 0
+        self.last_mark = 0
+        self.table_with_marks = []
+        self.women_distance = 0
+        self.men_distance = 0
+
     def load_data(self):
-        pass
+        self.number_of_marks,self.last_mark,self.women_distance,self.men_distance = map(int,input().split())
+        self.table_with_marks = list(map(int,input().split()))
+
