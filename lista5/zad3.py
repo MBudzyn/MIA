623651d6de0e4b63ba982689fc065c31bf041b01
Mateusz Budzyński <budzynekm09@gmail.com>
2023-11-04 15:22:59 +0100

    new method which is checking which distances can be created finished

--- lista5/zad3.py
+++ lista5/zad3.py
@@ -10,3 +10,34 @@ class Zad3():
         self.number_of_marks,self.last_mark,self.women_distance,self.men_distance = map(int,input().split())
         self.table_with_marks = list(map(int,input().split()))
 
+    def wich_distances_possible_to_create(self):
+        result = [None,None]
+        current_length = 0
+        for start_index in range(len(self.table_with_marks) -1):
+            if result[0] != None and result[1] != None:
+                break
+            current_index = start_index + 1
+            while self.table_with_marks[current_index] - self.table_with_marks[start_index] <= self.men_distance and current_index < len(self.table_with_marks) - 1:
+                current_length = self.table_with_marks[current_index] - self.table_with_marks[start_index]
+                if current_length == self.men_distance and result[0] == None:
+                    result[0] = self.men_distance
+                if current_length == self.women_distance and result[1] == None:
+                    result[1] = self.women_distance
+                current_index += 1
+
+        if result[0] == None and result[1] == None:
+            return []
+        elif result[0] == None:
+            return [result[1]]
+        elif result[1] == None:
+            return [result[0]]
+        else:
+            return result
+
+
+
+
+
+
+
+
